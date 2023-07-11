8db4f25c4986937d711ad7af10eeff1aae2f3e24
Distant22 <sindistant@gmail.com>
2023-07-11 16:42:22 +0800

    feat(friendRoutes):add routes for friend agree and delete API

--- students/steven/Canchu/routes/friendRoutes.js
+++ students/steven/Canchu/routes/friendRoutes.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const friendController = require('../controllers/friendController');
 const util = require('../utils/util')
 
-router.post('/:user_id/request', util.authorize_json, friendController.postRequest);
-router.get('/pending', util.authorize_json, friendController.getPending);
+// POST
+router.post('/:user_id/request', util.authorize_bearer, friendController.postRequest);
+router.post('/:friendship_id/agree', util.authorize_bearer, friendController.postAgree);
+
+// GET
+router.get('/pending', util.authorize_bearer, friendController.getPending);
+
+// DELETE
+router.delete('/:friendship_id', util.authorize_bearer, friendController.deleteFriend);
 
 module.exports = router;
