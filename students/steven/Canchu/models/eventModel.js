b93a01c4de800ba2fa15da8e5699b489715d995b
Distant22 <sindistant@gmail.com>
2023-07-12 15:02:45 +0800

    feat(eventModel):add model for event controller

---
+++ students/steven/Canchu/models/eventModel.js
@@ -0,0 +1,78 @@
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const util = require('../utils/util')
+
+const db = mysql.createConnection({
+	host: process.env.DB_HOST || 'localhost',
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: 'user'
+});
+
+db.connect((err) => {
+	if (err) {
+		throw err;
+	}
+	console.log('Event Model：Connected to MySQL database');
+});
+
+module.exports = {
+    getEvent: async(res,userId) => {
+        const sql = 'SELECT id, type, image, summary, is_read, created_at FROM event WHERE user_id = ?';
+        db.query(sql, [userId], (error, results) => {
+            if (error) {
+                console.error('Database error:', error);
+                return res.status(500).json({ error: 'Server error' });
+            }
+            const userEvent = results[0];
+            console.log("User Event Selected, results=",userEvent)
+            // Construct the response object
+            const response = {
+                data: {
+                    events: {
+                        id: userEvent.id,
+                        type: userEvent.type,
+                        is_read: userEvent.is_read,
+                        image: userEvent.image,
+                        created_at: userEvent.created_at,
+                        summary: userEvent.summary
+                    },
+                },
+            };
+            console.log("User Event get success, response=",response)
+            return res.status(200).json(response);
+        })
+    },
+    readEvent: async(res,userId,eventId) => {
+        const sql_validate = 'SELECT user_id FROM event WHERE id = ?';
+        db.query(sql_validate,[eventId], (error, results) => {
+            if (error) {
+                console.error('Database error:', error);
+                return res.status(500).json({ error: 'Server error' });
+            }
+            if (results[0].user_id !== userId){
+                console.error('錯誤！此欄的userId和Token的實際id分別為',results[0].user_id,userId)
+                return res.status(400).json({ error: 'This user has no permission to read event'})
+            } else {
+                console.log('回傳-',results[0])
+                const sql = 'UPDATE event SET is_read = true WHERE id = ?';
+                db.query(sql, [eventId], (error, results) => {
+                    if (error) {
+                        console.error('Database error:', error);
+                        return res.status(500).json({ error: 'Server error' });
+                    }
+                    // Construct the response object
+                    const response = {
+                        data: {
+                            events: {
+                                id: eventId
+                            },
+                        },
+                    };
+                    console.log("User Event get success, response=",response)
+                    return res.status(200).json(response);
+                })
+            }
+        })
+    }
+}
