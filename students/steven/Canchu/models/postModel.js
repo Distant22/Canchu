ffb170feff297f96730bfa3e4e4e0c96811e1065
Distant22 <sindistant@gmail.com>
2023-07-13 10:18:39 +0000

    fix(postModel):modify id return type

--- students/steven/Canchu/models/postModel.js
+++ students/steven/Canchu/models/postModel.js
@@ -198,7 +198,7 @@ module.exports = {
                                     const { id, text, comment_created_at, user_id, name, picture } = result
                                     console.log("結果：",result)
                                     return {
-                                        id: id,
+                                        id: parseInt(id,10),
                                         created_at : comment_created_at,
                                         content: text,
                                         user : {
