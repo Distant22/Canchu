5a7c0055443cb78956d507be703385e010182aa9
Distant22 <sindistant@gmail.com>
2023-07-13 15:41:25 +0800

    feat(postController):add needed controller

--- students/steven/Canchu/controllers/postController.js
+++ students/steven/Canchu/controllers/postController.js
@@ -6,10 +6,31 @@ module.exports = {
         const id = req.user.id
         await postModel.createPost(res,id,context)
     },
+    createLike: async(req,res) => {
+        const id = req.user.id
+        const post_id = req.params.id
+        await postModel.createLike(res,id,post_id)
+    },
+    createComment: async(req,res) => {
+        const { content } = req.body;
+        const id = req.user.id
+        const post_id = req.params.id
+        await postModel.createComment(res,id,post_id,content)
+    },
+    deleteLike: async(req,res) => {
+        const id = req.user.id
+        const post_id = req.params.id
+        await postModel.deleteLike(res,id,post_id)
+    },
     updatePost: async(req,res) => {
         const { context } = req.body;
         const id = req.user.id
         const post_id = req.params.id
         await postModel.updatePost(res,id,post_id,context)
+    },
+    getDetail: async(req,res) => {
+        const id = req.user.id
+        const post_id = req.params.id
+        await postModel.getDetail(res,id,post_id)
     }
 }
