e9cbe928931a3e8025dcac6cfad7968a36b41998
Distant22 <sindistant@gmail.com>
2023-07-13 02:43:45 +0000

    fix(postModel):modify return result id and code position

--- students/steven/Canchu/models/postModel.js
+++ students/steven/Canchu/models/postModel.js
@@ -24,7 +24,7 @@ module.exports = {
                 console.error('Database error:', error);
                 return res.status(500).json({ error: 'Server error' });
             }
-            const name = results
+            const name = results[0].name
             const postTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
             console.log("找到使用者名稱為：",name,"；發文內容為",context)
             const sql = 'INSERT INTO post ( user_id, created_at, context, name ) VALUES (?,?,?,?)';
@@ -52,28 +52,29 @@ module.exports = {
                 console.error('Database error:', error);
                 return res.status(500).json({ error: 'Server error' });
             }
-            const user_id = results
-            console.log("找到使用者ID為：",user_id, results.user_id,"；發文內容為：",context)
+            const user_id = results[0].user_id
+            console.log("找到使用者ID為：",user_id, "；發文內容為：",context)
             if(user_id !== id){
                 console.error('Database error:', error);
                 return res.status(400).json({ error: 'This user has no permission to update post' });
-            }
-        })
-        const sql = 'UPDATE post SET context = ? WHERE id = ?';
-        db.query(sql, [context, post_id], (error, results) => {
-            if (error) {
-                console.error('Database error:', error);
-                return res.status(500).json({ error: 'Server error' });
-            }
-            console.log("更新發文id",post_id)
-            const response = {
-                data: {
-                    post: {
-                        id: post_id
-                    }
-                },
-            };
-            return res.status(200).json(response);
+            } else {
+	    	const sql = 'UPDATE post SET context = ? WHERE id = ?';
+        	db.query(sql, [context, post_id], (error, results) => {
+            		if (error) {
+                		console.error('Database error:', error);
+                		return res.status(500).json({ error: 'Server error' });
+            		}
+            		console.log("更新發文id",post_id)
+            		const response = {
+                		data: {
+                    			post: {
+                        			id: post_id
+                    			}
+                		},
+            		};
+            		return res.status(200).json(response);
+        	})
+	    }
         })
     }
 }
