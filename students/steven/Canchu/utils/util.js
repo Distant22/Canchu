e6efb866a395f545a1025c1226e6d432f0f51c98
Distant22 <sindistant@gmail.com>
2023-07-14 15:01:23 +0800

    fix(util):rm unneeded import and logs

--- students/steven/Canchu/utils/util.js
+++ students/steven/Canchu/utils/util.js
@@ -1,22 +1,17 @@
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
 
     authorize_bearer: (req, res, next) => {
         const token = req.headers.authorization;
-        console.log("測試authorize：",req.get('content-type'))
         if (!token || !token.startsWith('Bearer ')) {
             return res.status(401).json({ error: 'No token provided' });
         }
         const accessToken = token.split(' ')[1];
         try {
             // Verify and decode the access token
-            const decoded = jwt.verify(accessToken, 'dt22');
-            req.user = decoded; // Attach the user information to the request object
-            console.log("解碼成功！")
+            const decoded = jwt.verify(accessToken, process.env.JWT_SIGN);
+            req.user = decoded;
             next();
         } catch (error) {
             return res.status(403).json({ error: 'Invalid token' });
