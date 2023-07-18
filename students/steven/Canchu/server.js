735a93fced03ef02ae5c2bfcd1bd2e764debc3f8
Distant22 <sindistant@gmail.com>
2023-07-18 05:26:17 +0000

    fix(server):add correct variable to show main page text

--- students/steven/Canchu/server.js
+++ students/steven/Canchu/server.js
@@ -24,7 +24,7 @@ app.use('/api/1.0/events', event_route);
 const post_route = require('./routes/postRoutes');
 app.use('/api/1.0/posts', post_route);
 
-router.get('/', (req, res) => {res.send('Main Page listening! -Dt22')})
+app.get('/', (req, res) => {res.send('Main Page listening! -Dt22')})
 
 app.get('/.well-known/pki-validation/AD335B614CF30912AE7C22F2D222450F.txt', (req, res) => {
   const filePath = path.join(__dirname, 'public', 'AD335B614CF30912AE7C22F2D222450F.txt');
