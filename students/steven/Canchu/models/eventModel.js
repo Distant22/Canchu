84c5abf6a7fecc06f10f0a3e54f3785fe9779e79
Distant22 <sindistant@gmail.com>
2023-07-20 15:16:26 +0800

    refactor(eventModel):change to sql2

--- students/steven/Canchu/models/eventModel.js
+++ students/steven/Canchu/models/eventModel.js
@@ -1,8 +1,7 @@
-const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const mysql = require('mysql2/promise');
 const util = require('../utils/util')
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
 	host: process.env.DB_HOST || 'localhost',
 	user: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
@@ -18,13 +17,9 @@ db.connect((err) => {
 
 module.exports = {
     getEvent: async(res,userId) => {
-        const sql = 'SELECT id, type, image, summary, is_read, created_at FROM event WHERE user_id = ? ORDER BY created_at DESC';
-        console.log("進入getEvent")
-        db.query(sql, [userId], (error, results) => {
-            if (error) {
-                console.error('Database error:', error);
-                return res.status(500).json({ error: 'Server error' });
-            }
+        try {
+            const sql = 'SELECT id, type, image, summary, is_read, created_at FROM event WHERE user_id = ? ORDER BY created_at DESC'
+            const [results] = await db.query(sql, [userId])
             const userEvent = results.map((result) => {
                 const {id, type, is_read, image, created_at, summary} = result
                 return {
@@ -43,39 +38,30 @@ module.exports = {
                 },
             };
             return res.status(200).json(response);
-        })
+        } catch (error) {
+            return util.databaseError(error,'getEvent',res);
+        }
     },
     readEvent: async(res,userId,eventId) => {
-	console.log("進入readEvent","userId=",userId,"eventId=",eventId)
-        const sql_validate = 'SELECT user_id FROM event WHERE id = ?';
-        db.query(sql_validate,[eventId], (error, results) => {
-            if (error) {
-                console.error('Database error:', error);
-                return res.status(500).json({ error: 'Server error' });
-            }
-	    console.log("結果：",results,results[0])
+        try {
+            const sql_validate = 'SELECT user_id FROM event WHERE id = ?';
+            const [results] = await db.query(sql_validate,[eventId])
             if (results[0].user_id !== userId){
-                console.error('錯誤！此欄的userId和Token的實際id分別為',results[0].user_id,userId)
                 return res.status(400).json({ error: 'This user has no permission to read event'})
             } else {
-                console.log('回傳-',results[0])
                 const sql = 'UPDATE event SET is_read = true WHERE id = ?';
-                db.query(sql, [eventId], (error, results) => {
-                    if (error) {
-                        console.error('Database error:', error);
-                        return res.status(500).json({ error: 'Server error' });
-                    }
-                    // Construct the response object
-                    const response = {
-                        data: {
-                            events: {
-                                id: eventId
-                            },
+                await db.query(sql, [eventId])
+                const response = {
+                    data: {
+                        events: {
+                            id: eventId
                         },
-                    };
-                    return res.status(200).json(response);
-                })
+                    },
+                };
+                return res.status(200).json(response);
             }
-        })
+        } catch (error) {
+            return util.databaseError(error,'getEvent',res);
+        }
     }
 }
