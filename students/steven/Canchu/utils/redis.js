a63f7a5f075b415ac05ad8b37ca663c89329f79a
Distant22 <sindistant@gmail.com>
2023-07-21 11:02:20 +0800

    feat(redis):add redis to new file

---
+++ students/steven/Canchu/utils/redis.js
@@ -0,0 +1,23 @@
+const Redis = require("ioredis");
+
+module.exports = {
+
+    get_redis: async (path,res) => {
+        const redis = new Redis();
+        const result = await redis.get(path,res)
+
+        if (result) {
+            console.log("Redis Cache found result!");
+            return res.status(200).json(result);
+        }
+    },
+
+    set_redis: async (path,data,res) => {
+        try {
+            const redis = new Redis();
+            await redis.set(path, data, 'EX', 3600)
+            console.log(await redis.hGetAll(path))
+        } catch (err) { return res.status(500).json({ error: 'Failed Caching' }); }
+    }
+
+}
