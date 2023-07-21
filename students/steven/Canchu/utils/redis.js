f4a5c2e62bfb084ca7f2d70b5ccacd6cf6d49061
Distant22 <sindistant@gmail.com>
2023-07-21 11:42:20 +0800

    fix redis

--- students/steven/Canchu/utils/redis.js
+++ students/steven/Canchu/utils/redis.js
@@ -5,11 +5,7 @@ module.exports = {
     get_redis: async (path,res) => {
         const redis = new Redis();
         const result = await redis.get(path,res)
-
-        if (result) {
-            console.log("Redis Cache found result!");
-            return res.status(200).json(result);
-        }
+        return result
     },
 
     set_redis: (path,data,res) => {
@@ -17,7 +13,7 @@ module.exports = {
             const redis = new Redis();
             redis.set(path, data, 'EX', 3600)
             redis.get(path).then((result) => {
-                console.log("Redis 取得成功，result：",result); // Prints "value"
+                console.log("Redis 設置成功，result：",result); // Prints "value"
             });
         } catch (err) { console.log("Error in redis! msg：", err ) }
     }
