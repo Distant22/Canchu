402bf89d8249c121e5c2c91b6f9db19694883e23
Distant22 <sindistant@gmail.com>
2023-07-24 16:28:06 +0800

    feat(server):add rate limiter

--- students/steven/Canchu/server.js
+++ students/steven/Canchu/server.js
@@ -1,19 +1,36 @@
 const express = require('express')
 const app = express()
-const router = express.Router();
 const port = 3000
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
+// Use Limiter
+const limiter = rateLimit({
+  windowMs: 1000,   // 1 second
+  max: 10,          // 10 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+});
+app.use(limiter);
+
+// Use CORS
 app.use(cors());
 
+// Use dotenv
 require('dotenv').config();
 
+// Use bodyparser
 app.use(bodyParser.json());
 
+// Use static
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.get('/.well-known/pki-validation/AD335B614CF30912AE7C22F2D222450F.txt', (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'AD335B614CF30912AE7C22F2D222450F.txt');
+  res.sendFile(filePath);
+});
 
+// Define all routes
 const user_route = require('./routes/userRoutes');
 app.use('/api/1.0/users', user_route);
 
@@ -27,11 +44,6 @@ const post_route = require('./routes/postRoutes');
 app.use('/api/1.0/posts', post_route);
 
 app.get('/', (req, res) => {res.send('Main Page listening! -Dt22')})
-
-app.get('/.well-known/pki-validation/AD335B614CF30912AE7C22F2D222450F.txt', (req, res) => {
-  const filePath = path.join(__dirname, 'public', 'AD335B614CF30912AE7C22F2D222450F.txt');
-  res.sendFile(filePath);
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
