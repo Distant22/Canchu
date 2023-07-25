de59ffbc51cb9946b6c82e982daf28dd586f9730
Distant22 <sindistant@gmail.com>
2023-07-25 10:46:05 +0800

    feat(package):add jest mysql

--- students/steven/Canchu/jest-mysql-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    databaseOptions: {
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: "user"
-    },
-  };
