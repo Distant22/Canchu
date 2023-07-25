be19a4e9121943a6e770b9f2f27faf073cda8ce6
Distant22 <sindistant@gmail.com>
2023-07-25 16:53:17 +0800

    fix(util):add async to delete redis

--- students/steven/Canchu/tests/userModel.test.js
+++ students/steven/Canchu/tests/userModel.test.js
@@ -13,6 +13,9 @@ afterAll((done) => {
   }
 });
 
+// ---------------------------- Signin --------------------------------------------------------
+
+// Signin｜測試成功情況（200）
 describe("POST /api/1.0/users/signin", () => {
   it("should signin", async () => {
 
@@ -22,10 +25,84 @@ describe("POST /api/1.0/users/signin", () => {
       provider: "native",
     });
     expect(res.statusCode).toBe(200);
-    token = res.body.data.access_token;
+    // token = res.body.data.access_token;
   });
 });
 
+// Signin｜測試Provider未提供情況（400）
+describe("POST /api/1.0/users/signin", () => {
+  it("should signin", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signin").send({
+      email: "Steven@gmail.com",
+      password: "123"
+    });
+    expect(res.statusCode).toBe(400);
+  });
+});
+
+// Signin｜測試Provider提供錯誤情況（403）
+describe("POST /api/1.0/users/signin", () => {
+  it("should signin", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signin").send({
+      email: "Steven@gmail.com",
+      password: "123",
+      provider: "wrong",
+    });
+    expect(res.statusCode).toBe(403);
+  });
+});
+
+// Signin｜測試臉書登入未提供Token情況（400）
+describe("POST /api/1.0/users/signin", () => {
+  it("should signin", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signin").send({
+      provider: "facebook"
+    });
+    expect(res.statusCode).toBe(400);
+  });
+});
+
+// Signin｜測試沒有密碼/Email情況（400）
+describe("POST /api/1.0/users/signin", () => {
+  it("should signin", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signin").send({
+      email: "Steven@gmail.com",
+      provider: "native"
+    });
+    expect(res.statusCode).toBe(400);
+  });
+});
+describe("POST /api/1.0/users/signin", () => {
+  it("should signin", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signin").send({
+      password: "123",
+      provider: "native"
+    });
+    expect(res.statusCode).toBe(400);
+  });
+});
+
+// Signin｜測試密碼錯誤情況（403）
+describe("POST /api/1.0/users/signin", () => {
+  it("should signin", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signin").send({
+      email: "Steven@gmail.com",
+      password: "dhcldskcdskc",
+      provider: "native",
+    });
+    expect(res.statusCode).toBe(403);
+  });
+});
+
+// ---------------------------- Signup --------------------------------------------------------
+
+// Signup｜測試成功情況（200）
 describe("POST /api/1.0/users/signup", () => {
   it("should signup", async () => {
 
@@ -38,27 +115,41 @@ describe("POST /api/1.0/users/signup", () => {
   });
 });
 
-describe("GET /api/1.0/users/407/profile", () => {
-  it("should get profile", async () => {
-
-    const res = await request(app)
-    .get("/api/1.0/users/407/profile")
-    .set('Authorization', `Bearer ${token}`);
-    expect(res.statusCode).toBe(200);
-  });
-});
-
-describe("PUT /api/1.0/users/profile", () => {
+// Signup｜測試欄位缺失情況（400）
+describe("POST /api/1.0/users/signup", () => {
   it("should signup", async () => {
 
-    const res = await request(app)
-    .put("/api/1.0/users/profile")
-    .set('Authorization', `Bearer ${token}`)
-    .send({
-      name: util.generateRandomString(5),
-      introduction: util.generateRandomString(10),
-      tags: util.generateRandomString(5)
+    const res = await request(app).post("/api/1.0/users/signup").send({
+      name: util.generateRandomString(8),
+      password: "123",
     });
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(400);
   });
 });
+
+// Signup｜測試信箱格式出錯情況（400）
+describe("POST /api/1.0/users/signup", () => {
+  it("should signup", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signup").send({
+      name: util.generateRandomString(8),
+      password: "123",
+      email: util.generateRandomString(8)
+    });
+    expect(res.statusCode).toBe(400);
+  });
+});
+
+// Signup｜測試Email已被使用情況（403）
+describe("POST /api/1.0/users/signup", () => {
+  it("should signup", async () => {
+
+    const res = await request(app).post("/api/1.0/users/signup").send({
+      name: util.generateRandomString(8),
+      password: "123",
+      email: "Steven@gmail.com",
+    });
+    expect(res.statusCode).toBe(403);
+  });
+});
+
