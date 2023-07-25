8769ae235c380b6c95fb70fff60d22b8ff218fe0
Distant22 <sindistant@gmail.com>
2023-07-25 14:26:16 +0800

    fix(userModel.test):adjust test

---
+++ students/steven/Canchu/jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+
+module.exports = {
+    // ... Other Jest configuration options ...
+  
+    // Use the testMatch option with an array of file patterns to match multiple test files
+    testMatch: ['**/tests/userModel.test.js', '**/tests/postModel.test.js'],
+  };
+  
