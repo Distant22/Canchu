a15048627dbf009140130b4b92c9c763050564f2
Distant22 <sindistant@gmail.com>
2023-07-26 14:03:18 +0800

    fix(userModel.test):modify test setting

--- students/steven/Canchu/tests/userModel.test.js
+++ students/steven/Canchu/tests/userModel.test.js
@@ -10,7 +10,7 @@ var token;
 
 jest.mock('../utils/util', () => ({
   db: {
-    query: jest.fn().mockResolvedValue(1) // Assuming the query returns the insertId
+    query: jest.fn() // Assuming the query returns the insertId
   }
 }));
 
