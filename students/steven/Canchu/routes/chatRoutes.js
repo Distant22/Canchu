c0ae370a8f6f18252d4dde37d40ca7533d954910
Distant22 <sindistant@gmail.com>
2023-07-28 16:08:18 +0800

    feat:chat

--- students/steven/Canchu/routes/chatRoutes.js
+++ students/steven/Canchu/routes/chatRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const chatController = require('../controllers/chatController');
 const util = require('../utils/util')
 
-router.get('/:user_id/message', [util.authorize_bearer], chatController.search);
+router.get('/:user_id/message(s)?', [util.authorize_bearer], chatController.search);
 
 router.post('/:user_id', [util.authorize_bearer,util.authorize_json], chatController.sendMessage);
 
