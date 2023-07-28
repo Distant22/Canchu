8812296086e0c0244d60aeea412afe7335b67c05
Distant22 <sindistant@gmail.com>
2023-07-28 14:12:23 +0800

    fix(groupModel):modify code

--- students/steven/Canchu/models/groupModel.js
+++ students/steven/Canchu/models/groupModel.js
@@ -25,18 +25,14 @@ module.exports = {
         try {
             console.log("刪除社團：社團ID為",id)
 
-            const search_sql = 'SELECT * FROM group_data WHERE id = ?'
-            const [search_result] = await db.query(search_sql, [id])
-            console.log("刪除驗證：",search_result)
-            if(search_result.length === 0){ 
+            const search_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
+            const [validate_result] = await db.query(search_sql, [id])
+            console.log("刪除驗證：",validate_result)
+            if(validate_result.length === 0){ 
                 return res.status(400).json({ error: `There's no such group.` });
+            } else if(validate_result[0].creator_id !== user_id){ 
+                return res.status(400).json({ error: `You have no permission to delete this.` });
             } else {
-
-                const validate_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
-                const [validate_result] = await db.query(validate_sql, [id])
-                if(validate_result[0].creator_id !== user_id){ 
-                    return res.status(400).json({ error: `You have no permission to delete this.` });
-                }
 
                 const sql = 'DELETE FROM group_data WHERE id = ?'
                 await db.query(sql, [id])
@@ -94,19 +90,15 @@ module.exports = {
     // id：Token持有者；group_id：他想看的社團ID
     getPending: async(res,id,group_id) => {
         try {
-
-            const search_sql = 'SELECT * FROM group_data WHERE id = ?'
-            const [search_result] = await db.query(search_sql, [group_id])
-            console.log("取得Pending驗證：",search_result)
-            if(search_result.length === 0){ 
+            
+            const search_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
+            const [validate_result] = await db.query(search_sql, [group_id])
+            console.log("取得Pending驗證：",validate_result)
+            if(validate_result.length === 0){ 
                 return res.status(400).json({ error: `There's no such group.` });
+            } else if(validate_result[0].creator_id !== id){ 
+                return res.status(400).json({ error: `You have no permission to see this.` });
             } else {
-
-                const validate_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
-                const [validate_result] = await db.query(validate_sql, [group_id])
-                if(validate_result[0].creator_id !== id){ 
-                    return res.status(400).json({ error: `You have no permission to see this.` });
-                }
 
                 const sql = `SELECT u.id, u.name, u.picture, g.status FROM users AS u LEFT JOIN groupMember AS g ON u.id = g.user_id WHERE g.status = 'pending' AND g.group_id = ?`
                 const [results_list] = await db.query(sql, [group_id])
@@ -138,18 +130,13 @@ module.exports = {
     agreeJoin: async(res,id,user_id,group_id) => {
         try {
             console.log("agreeJoin：社團ID為",group_id,"使用者為",user_id,"Token ID為",id)
-            // 1.確認有無此社團
-            const search_sql = 'SELECT * FROM group_data WHERE id = ?'
-            const [search_result] = await db.query(search_sql, [group_id])
-            console.log("agreeJoin - 有無社團驗證：",search_result)
-            if(search_result.length === 0){ 
+            // 1.確認有無此社團 2.確認我是否有權限
+            const search_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
+            const [validate_result] = await db.query(search_sql, [group_id])
+            console.log("agreeJoin - 有無社團驗證：",validate_result)
+            if(validate_result.length === 0){ 
                 return res.status(400).json({ error: `There's no such group.` });
-            }
-
-            // 2.確認我是否有權限
-            const validate_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
-            const [validate_result] = await db.query(validate_sql, [group_id])
-            if(validate_result[0].creator_id !== id){ 
+            } else if(validate_result[0].creator_id !== id){ 
                 return res.status(400).json({ error: `You have no permission to agree this.` });
             }
 
@@ -176,6 +163,46 @@ module.exports = {
 
         } catch (error) {
             return util.databaseError(error,'agreeJoin',res);
+        }
+    },
+    postGroup: async(res,id,group_id,context) => {
+        try {
+            console.log("postGroup：社團ID為",group_id,"內文為",context,"Token ID為",id)
+            const search_sql = 'SELECT creator_id FROM group_data WHERE id = ?'
+            const [validate_result] = await db.query(search_sql, [group_id])
+            console.log("agreeJoin - 有無社團驗證：",validate_result)
+            if(validate_result.length === 0){ 
+                return res.status(400).json({ error: `There's no such group.` });
+            } else if(validate_result[0].creator_id !== id){ 
+                return res.status(400).json({ error: `You have no permission to post this.` });
+            }
+            const postTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            const update_sql = `
+            WITH my_info AS (
+                SELECT picture, name FROM users WHERE id = ?
+            )
+            INSERT INTO groupPost (group_id, user_id, created_at, context, picture, name)
+            SELECT ?, ?, ?, ?, picture, name
+            FROM my_info;
+            `
+            const [results] = await db.query(update_sql, [id,group_id,id,postTime,context])
+            const resultID = results.insertId;
+            const response = {
+                data: {
+                    group: {
+                        id: group_id
+                    },
+                    user: {
+                        id: id
+                    },
+                    post: {
+                        id: resultID
+                    }
+                },
+            };
+            return res.status(200).json(response);
+        } catch (error) {
+            return util.databaseError(error,'postGroup',res);
         }
     }
 }
