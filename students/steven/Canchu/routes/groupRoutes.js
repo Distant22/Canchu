0b1fb35ac7156255f052b05517cdc3b6e066202a
Distant22 <sindistant@gmail.com>
2023-07-28 13:18:52 +0800

    fix(groupModel):modify sql statement

--- students/steven/Canchu/routes/groupRoutes.js
+++ students/steven/Canchu/routes/groupRoutes.js
@@ -5,11 +5,9 @@ const util = require('../utils/util')
 
 router.get('/:group_id/member/pending', [util.authorize_bearer], groupController.getPending);
 
-router.post('/', [util.authorize_bearer,util.authorize_json], groupController.createGroup );
-
 router.post('/:group_id/member/:user_id/agree', [util.authorize_bearer], groupController.agreeJoin );
-
 router.post('/:group_id/join', [util.authorize_bearer], groupController.joinGroup );
+router.post('/', [util.authorize_bearer,util.authorize_json], groupController.createGroup );
 
 router.delete('/:id', [util.authorize_bearer], groupController.deleteGroup );
 
