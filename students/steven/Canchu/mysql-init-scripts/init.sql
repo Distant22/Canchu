e7bdafc2fe5e452bbc89266469d72b00e2da6260
Distant22 <sindistant@gmail.com>
2023-08-04 09:21:42 +0000

    fix(Canchu):adjust dockerfile and sql

--- students/steven/Canchu/mysql-init-scripts/init.sql
+++ students/steven/Canchu/mysql-init-scripts/init.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `event`
 --
 
+use user;
+
+
 DROP TABLE IF EXISTS `event`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
